d5262612fec8b035b77a1bc756fadf8cc5aa21e3
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-13 11:57:41 +0900

    update

--- hello.php
+++ hello.php
@@ -1,11 +1,24 @@
+<?php
+//PHPプログラムを記述
+$message = "こんにちは";
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h2>Hello</h2>
+    <p>
+        <?php echo $message ?>
+    </p>
+    <p>
+        <?= $message ?>
+    </p>
 </body>
+
 </html>
