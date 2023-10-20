61938c4b055ed67efb1b34924efb3e3c21ead4ca
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-20 10:46:36 +0900

    update

--- regist/confirm.php
+++ regist/confirm.php
@@ -13,6 +13,21 @@ $_SESSION['regist'] = $regist;
 
 $genders['male'] = "男性";
 $genders['female'] = "女性";
+
+
+/**
+ * サニタイズ
+ */
+function check($posts)
+{
+    if (empty($posts)) return;
+    foreach ($posts as
+        $column => $post) {
+        $posts[$column] =
+            htmlspecialchars($post, ENT_QUOTES, 'UTF-8');
+    }
+    return $posts;
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,27 +41,29 @@ $genders['female'] = "女性";
     <div class="container">
         <h1>確認画面</h1>
         <p>この内容で登録してもよろしいですか？</p>
-        <form action="store.php" method="post">
-            <div>
-                <label for="">氏名</label>
-                <?= $regist['name'] ?>
-            </div>
-            <div>
-                <label for="">Email</label>
-                <?= $regist['email'] ?>
-            </div>
-            <div>
-                <label for="">生年月日</label>
-                <?= $regist['year'] ?>/<?= $regist['month'] ?>/<?= $regist['day'] ?>
-            </div>
-            <div>
-                <label for="">性別</label>
-                <?= $genders[$regist['gender']] ?>
-            </div>
-            <button class="btn btn-primary">登録</button>
+        <div>
+            <form action="store.php" method="post">
+                <div>
+                    <label for="">氏名</label>
+                    <?= $regist['name'] ?>
+                </div>
+                <div>
+                    <label for="">Email</label>
+                    <?= $regist['email'] ?>
+                </div>
+                <div>
+                    <label for="">生年月日</label>
+                    <?= $regist['year'] ?>/<?= $regist['month'] ?>/<?= $regist['day'] ?>
+                </div>
+                <div>
+                    <label for="">性別</label>
+                    <?= $genders[$regist['gender']] ?>
+                </div>
+                <button class="btn btn-primary">登録</button>
 
-            <a class="btn btn-outline-primary" href="input.php">戻る</a>
-        </form>
+                <a class="btn btn-outline-primary" href="input.php">戻る</a>
+            </form>
+        </div>
     </div>
 </body>
 
